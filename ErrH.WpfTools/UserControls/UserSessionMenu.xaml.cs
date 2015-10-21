58f17c004af415015788cabca1aa2bf5d84b507e
Peterson Salamat <petersonsalamat@gmail.com>
2015-10-21 17:04:48 +0800

    Backup Before Biyahe

--- ErrH.WpfTools/UserControls/UserSessionMenu.xaml.cs
+++ ErrH.WpfTools/UserControls/UserSessionMenu.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ErrH.Tools.Extensions;
 using ErrH.WpfTools.Extensions;
-using ErrH.WpfTools.ViewModels;
 
 namespace ErrH.WpfTools.UserControls
 {
@@ -27,8 +13,14 @@ namespace ErrH.WpfTools.UserControls
         {
             InitializeComponent();
 
-            Loaded += (s, e) 
-                => tbxPwd.DoLogin(btnLogin.Command, lblPwd);
+            Loaded += (s, e) =>
+            {
+                inputsPanel.IsVisibleChanged += (t, f) 
+                    => Keyboard.ClearFocus();
+
+                tbxPwd.DoLogin(btnLogin.Command, lblPwd);
+            };
+
         }
     }
 }
