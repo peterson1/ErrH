0f94577aede9af7bf6266feeb19eda8c2e1a71e8
Peterson Salamat <petersonsalamat@gmail.com>
2016-02-01 16:58:39 +0800

    Added validation helpers.

---
+++ ErrH.Tools/DataErrorInfoHelpers/DataErrorInfoHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ErrH.Tools.Extensions;
+
+namespace ErrH.Tools.DataErrorInfoHelpers
+{
+    public class DataErrorInfoHelper
+    {
+        private Dictionary<string, string> _errors = new Dictionary<string, string>();
+
+
+        public string ErrorText => string.Join(L.f, _errors.Values);
+
+
+
+
+        public string MessageIfBlank(string value, string label, string colName)
+            => ErrorIf(value, x => x.IsBlank(),
+                $"{label} must not be blank.", colName);
+
+
+        public string MessageIfNoValue(DateTime? value, string label, string colName)
+            => ErrorIf(value, x => !x.HasValue,
+                $"{label} must have a value.", colName);
+
+
+        public string MessageIfNull<T>(T value, string label, string colName)
+            => ErrorIf(value, x => x == null,
+                $"{label} must have a value.", colName);
+
+
+
+
+        private string ErrorIf<T>(T value,
+                                  Func<T, bool> func,
+                                  string message,
+                                  string colName)
+        {
+            if (!func.Invoke(value))
+            {
+                if (_errors.ContainsKey(colName))
+                    _errors.Remove(colName);
+                return null;
+            }
+
+            if (_errors.ContainsKey(colName))
+                _errors[colName] = message;
+            else
+                _errors.Add(colName, message);
+
+            return message;
+        }
+    }
+}
