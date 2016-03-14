d1327d571804428cd5f099c8ab939af3dbd18373
Peterson Salamat <petersonsalamat@gmail.com>
2016-03-14 16:58:03 +0800

    Added a converter.

---
+++ ErrH.Wpf.net45/Converters/VisibleIfFalseConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ErrH.Wpf.net45.Converters
+{
+    public class VisibleIfFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (bool)value == false ? Visibility.Visible : Visibility.Collapsed;
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
