74c4e34cbc37845dbd6db397e34b06fdb9c68332
Peterson Salamat <petersonsalamat@gmail.com>
2016-04-06 13:01:00 +0800

    Copied LabeledText ctrl to new Wpf.net45.

---
+++ ErrH.Wpf.net45/UserControls/LabeledText.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using AutoDependencyPropertyMarker;
+
+namespace ErrH.Wpf.net45.UserControls
+{
+    [AutoDependencyProperty]
+    public partial class LabeledText : UserControl
+    {
+        public string      Label       { get; set; }
+        public string      Text        { get; set; }
+
+        public FontWeight  LabelWeight { get; set; }
+        public FontWeight  TextWeight  { get; set; }
+
+        public Brush       LabelBrush  { get; set; }
+        public Brush       TextBrush   { get; set; }
+
+        public GridLength  LabelWidth  { get; set; }
+        public GridLength  GapWidth    { get; set; }
+        public GridLength  TextWidth   { get; set; }
+
+
+
+        public LabeledText()
+        {
+            InitializeComponent();
+        }
+    }
+}
