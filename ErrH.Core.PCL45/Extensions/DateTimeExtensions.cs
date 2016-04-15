d10f92a97c9346e0147f5fbf0db457e935fd4832
Peterson Salamat <petersonsalamat@gmail.com>
2016-04-15 16:43:44 +0800

    New date extension: IfEarlierThan().

---
+++ ErrH.Core.PCL45/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ErrH.Core.PCL45.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime IfEarlierThan (this DateTime date1, DateTime date2)
+            => date1.Ticks < date2.Ticks ? date1 : date2;
+    }
+}
