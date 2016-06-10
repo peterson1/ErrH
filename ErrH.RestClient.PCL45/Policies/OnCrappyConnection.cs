94c7840272e9623cfe309d2057351b97ace33a2e
Peterson Salamat <petersonsalamat@gmail.com>
2016-06-10 18:50:14 +0800

    Added InternalServerError to CrappyConnection candidates.

--- ErrH.RestClient.PCL45/Policies/OnCrappyConnection.cs
+++ ErrH.RestClient.PCL45/Policies/OnCrappyConnection.cs
@@ -2,13 +2,24 @@
 using System.Net;
 using System.Threading;
 using Polly;
+using ServiceStack;
 
 namespace ErrH.RestClient.PCL45.Policies
 {
     public class OnCrappyConnection
     {
+        //public static Policy RetryForever(string baseURL, int retryDelaySeconds, Action<string> errorNotifier = null)
+        //    => Policy.Handle<WebException>(x => IsCrappy(x)).RetryForeverAsync(ex =>
+        //    {
+        //        errorNotifier?.Invoke(FormatError(ex, baseURL, retryDelaySeconds));
+
+        //        using (EventWaitHandle tmpEvent = new ManualResetEvent(false))
+        //        {
+        //            tmpEvent.WaitOne(TimeSpan.FromSeconds(retryDelaySeconds));
+        //        }
+        //    });
         public static Policy RetryForever(string baseURL, int retryDelaySeconds, Action<string> errorNotifier = null)
-            => Policy.Handle<WebException>(x => IsCrappy(x)).RetryForeverAsync(ex =>
+            => Policy.Handle<Exception>(x => IsCrappy(x)).RetryForeverAsync(ex =>
             {
                 errorNotifier?.Invoke(FormatError(ex, baseURL, retryDelaySeconds));
 
@@ -30,15 +41,38 @@ namespace ErrH.RestClient.PCL45.Policies
         }
 
 
-        private static bool IsCrappy(WebException ex)
+        //private static bool IsCrappy(WebException ex)
+        //{
+        //    if (ex.Status == WebExceptionStatus.ConnectFailure) return true;
+        //    if (ex.Status == WebExceptionStatus.SendFailure) return true;
+        //    if (ex.Message.ToLower().Contains("could not be resolved")) return true;//NameResolutionFailure
+        //    if (ex.Message.ToLower().Contains("kept alive")) return true;
+        //    if (ex.Message.ToLower().Contains("timed out")) return true;
+        //    if (ex.Message.ToLower().Contains("connection was closed")) return true;
+        //    if (ex.Message.ToLower().Contains("deadlock")) return true;
+        //    return false;
+        //}
+        private static bool IsCrappy(Exception ex)
         {
-            if (ex.Status == WebExceptionStatus.ConnectFailure) return true;
-            if (ex.Status == WebExceptionStatus.SendFailure) return true;
-            if (ex.Message.ToLower().Contains("could not be resolved")) return true;//NameResolutionFailure
-            if (ex.Message.ToLower().Contains("kept alive")) return true;
-            if (ex.Message.ToLower().Contains("timed out")) return true;
-            if (ex.Message.ToLower().Contains("connection was closed")) return true;
-            if (ex.Message.ToLower().Contains("deadlock")) return true;
+            var we = ex as WebException;
+            if (we != null)
+            {
+                if (we.Status == WebExceptionStatus.ConnectFailure) return true;
+                if (we.Status == WebExceptionStatus.SendFailure) return true;
+                if (we.Message.ToLower().Contains("could not be resolved")) return true;//NameResolutionFailure
+                if (we.Message.ToLower().Contains("kept alive")) return true;
+                if (we.Message.ToLower().Contains("timed out")) return true;
+                if (we.Message.ToLower().Contains("connection was closed")) return true;
+                if (we.Message.ToLower().Contains("deadlock")) return true;
+            }
+
+            var wse = ex as WebServiceException;
+            if (wse != null)
+            {
+                if (wse.Message.ToLower().Contains("deadlock")) return true;
+                if (wse.Message.ToLower().Contains("internal server error")) return true;
+            }
+
             return false;
         }
     }
