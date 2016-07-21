715ecf048d26598957dd28f092bbd4990c26f019
Peterson Salamat <petersonsalamat@gmail.com>
2016-07-22 02:17:22 +0800

    Added model for ChromeApp settings.

---
+++ ErrH.Core.PCL45/Configuration/ChromeAppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace ErrH.Core.PCL45.Configuration
+{
+    public class ChromeAppConfig
+    {
+        public string   ExePath      { get; set; }
+        public string   ProfileDir   { get; set; }
+        public string   NodeEditURL  { get; set; }
+    }
+}
