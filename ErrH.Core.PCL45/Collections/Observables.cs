3f2dcf3fa790a552418e280c49a176606aab2344
Peterson Salamat <petersonsalamat@gmail.com>
2016-08-16 16:33:48 +0800

    Added RowsSummarized event and trigger to serve as cue to update a totals row.

--- ErrH.Core.PCL45/Collections/Observables.cs
+++ ErrH.Core.PCL45/Collections/Observables.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using ErrH.Core.PCL45.Extensions;
 
 namespace ErrH.Core.PCL45.Collections
 {
@@ -9,7 +11,21 @@ namespace ErrH.Core.PCL45.Collections
         public Observables(IEnumerable<T> list) : base(list) { }
 
 
-        public Observables<T> SummaryRow { get; set; }
+        private      EventHandler _rowsSummarized;
+        public event EventHandler  RowsSummarized
+        {
+            add    { _rowsSummarized -= value; _rowsSummarized += value; }
+            remove { _rowsSummarized -= value; }
+        }
+
+
+        private Observables<T> _summaryRow;
+
+        public Observables<T> SummaryRow
+        {
+            get { return _summaryRow;  }
+            set { _summaryRow = value; _rowsSummarized?.Raise(); }
+        }
 
 
         public void Add(IEnumerable<T> items)
