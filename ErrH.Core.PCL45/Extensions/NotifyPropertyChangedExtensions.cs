0898e8c13f4f4cc8dbefed604f8344a34eae1a29
Pete as dev1 @FATS-SVR2 <dev1@FATS-SVR2>
2016-08-29 16:54:44 +0800

    Added shorthand method for adding OnChange handlers of INotifyPropertyChanged objects.

---
+++ ErrH.Core.PCL45/Extensions/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ErrH.Core.PCL45.Extensions
+{
+    public static class NotifyPropertyChangedExtensions
+    {
+        public static void OnChange(this INotifyPropertyChanged obj, string propertyName, Action action)
+        {
+            obj.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == propertyName) action?.Invoke();
+            };
+        }
+
+    }
+}
