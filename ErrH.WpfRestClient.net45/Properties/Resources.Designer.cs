944029d61141587d360cea7bd23cffabec6f1f3e
Peterson Salamat <petersonsalamat@gmail.com>
2016-12-10 07:44:39 +0800

    Retargeted all projects to framework 4.5.

--- ErrH.WpfRestClient.net45/Properties/Resources.Designer.cs
+++ ErrH.WpfRestClient.net45/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace ErrH.WpfRestClient.net45.Properties {
+    using System;
     
     
     /// <summary>
@@ -37,7 +38,7 @@ namespace ErrH.WpfRestClient.net45.Properties {
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ErrH.WpfRestClient.net45.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
