963375ea8c1017102efe126e8e1040bff9d40bef
Peterson Salamat <petersonsalamat@gmail.com>
2016-12-26 04:49:04 +0800

    Moved columns toggler method to Repo2.

--- ErrH.Wpf.net45/Extensions/DataGridExtensions.cs
+++ ErrH.Wpf.net45/Extensions/DataGridExtensions.cs
@@ -12,25 +12,25 @@ namespace ErrH.Wpf.net45.Extensions
 {
     public static class DataGridExtensions
     {
-        public static void AddColHeaderMenuItems(this DataGrid dg, 
-            string contxtMenuKey, DataGridLengthUnitType colWidthType = DataGridLengthUnitType.SizeToCells)
-        {
-            var cm = dg.ColumnHeaderStyle.Resources[contxtMenuKey] as ContextMenu;
-            cm.Items.Clear();
+        //public static void AddColHeaderMenuItems(this DataGrid dg, 
+        //    string contxtMenuKey, DataGridLengthUnitType colWidthType = DataGridLengthUnitType.SizeToCells)
+        //{
+        //    var cm = dg.ColumnHeaderStyle.Resources[contxtMenuKey] as ContextMenu;
+        //    cm.Items.Clear();
 
-            foreach (var col in dg.Columns)
-            {
-                var mnu = new MenuItem();
-                mnu.Header = col.Header;
-                mnu.IsCheckable = true;
-                mnu.IsChecked = col.Visibility == Visibility.Visible;
-                mnu.Click += (s, e) => col.Visibility = mnu.IsChecked
-                            ? Visibility.Visible : Visibility.Collapsed;
-                cm.Items.Add(mnu);
-                col.Width = new DataGridLength(0, colWidthType);
-            }
+        //    foreach (var col in dg.Columns)
+        //    {
+        //        var mnu = new MenuItem();
+        //        mnu.Header = col.Header;
+        //        mnu.IsCheckable = true;
+        //        mnu.IsChecked = col.Visibility == Visibility.Visible;
+        //        mnu.Click += (s, e) => col.Visibility = mnu.IsChecked
+        //                    ? Visibility.Visible : Visibility.Collapsed;
+        //        cm.Items.Add(mnu);
+        //        col.Width = new DataGridLength(0, colWidthType);
+        //    }
 
-        }
+        //}
 
 
         public static void SummarizeAt<T>(this DataGrid source,
